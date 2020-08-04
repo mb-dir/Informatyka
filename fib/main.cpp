4c82ea27df353ff7dbe7313ff8a0121a8e9f5846
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-08-04 20:30:55 +0200

    Sprawdzenie czy dana liczba nalezy do ciągu

--- fib/main.cpp
+++ fib/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-
+int liczba_do_sprawdzenia;
 
 //rekurencyjnie
 int fib_rek(int n){
@@ -20,12 +20,31 @@ int fib_iteracyjnie(int ktory){
     }
     return b;
 }
+
+//fibonaci iteracyjnie - sprawdzenie czy dana liczba nale¿y do ci¹gu
+void czy_nalezy(int liczba){
+    int a, b=1;
+    for(int i = 1; i < liczba; i++){
+        b+=a;
+        if(b == liczba){
+           cout<<liczba<<" nalezy do ciagu i jest to "<< i <<" wyraz";
+           return;
+        }
+        a=b-a;
+    }
+    cout<<liczba<<" nie nalezy do ciagu";
+}
 int main()
 {
-    for(int i = 1; i <=20; i++){
+    /*for(int i = 1; i <=20; i++){
         int t = fib_iteracyjnie(i);
         cout<<"iteracyjnie element: "<<i<<" to: "<<t<<endl;
         cout<<"rekurencynie element: "<<i<<" to: "<<fib_rek(i)<<endl;
-    }
+    }*/
+
+    cout<<"Podaj liczbe, a program sprawdzi czy jest ona z ciagu: ";
+    cin>>liczba_do_sprawdzenia;
+
+    czy_nalezy(liczba_do_sprawdzenia);
     return 0;
 }
