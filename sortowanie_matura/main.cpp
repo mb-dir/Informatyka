6b7fe73e08976c5e04c1f82462042fb46ec47214
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-08-20 16:15:50 +0200

    quicksort implementacja

--- sortowanie_matura/main.cpp
+++ sortowanie_matura/main.cpp
@@ -2,13 +2,17 @@
 
 using namespace std;
 
-int tab[7] = {3,1,12,41,12, 1519, -12};
+int tab[6] = {12,-2,4,3,17,16};
 
-void sortowanie_babelkowe(int *tab, int n){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n-1; j++){
-            if(tab[j] > tab[j+1]){
-                int bf = tab[j+1];//zamiana wartoúci
+void sortowanie_babelkowe(int *tab, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n-1; j++)
+        {
+            if(tab[j] > tab[j+1])
+            {
+                int bf = tab[j+1];//zamiana warto≈ìci
                 tab[j+1] = tab[j];
                 tab[j] = bf;
             }
@@ -16,11 +20,39 @@ void sortowanie_babelkowe(int *tab, int n){
     }
 }
 
-int main()
+void quicksort(int *tablica, int lewy, int prawy)
 {
-    sortowanie_babelkowe(tab, 7);
-    //wypisanie w celu upewnienia sie ze dziala
-    for(int a = 0; a < 7; a++){
+    int v=tablica[(lewy+prawy)/2];
+    int i,j,x;
+    i=lewy;
+    j=prawy;
+    do
+    {
+        while (tablica[i]<v) i++;
+        while (tablica[j]>v) j--;
+        if (i<=j)
+        {
+            x=tablica[i];
+            tablica[i]=tablica[j];
+            tablica[j]=x;
+            i++;
+            j--;
+        }
+    }while (i<=j);
+
+    if (j>lewy) quicksort(tablica,lewy, j);
+    if (i<prawy) quicksort(tablica, i, prawy);
+}
+
+int main(){
+    //sortowanie_babelkowe(tab, 6);
+
+    /*for(int a = 0; a < 7; a++){
+        cout<<tab[a]<<endl;
+    }*/
+    quicksort(tab, 0, 5);//5 - index ostatniego elementu tablicy
+    for(int a = 0; a < 6; a++)
+    {
         cout<<tab[a]<<endl;
     }
     return 0;
