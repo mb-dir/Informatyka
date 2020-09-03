e5144746db3eb36a168c13a592e55ab8b956b040
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-09-03 16:57:34 +0200

    Poprawa algorytmu
    Wdrożenie zmian na które wpadłem w szkole

--- wyszukiwanie_binarne/main.cpp
+++ wyszukiwanie_binarne/main.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 //posortowany zbior elementow
-int tab[10] = {2,4,6,8,10,12,14,16,18,20};
+int tab[10] = {3,4,5,8,10,12,14,16,18,19};
 
 void wyszukiwanie_binarne(int* tab, int index_l, int index_p, int szukany){
     int srodek = (index_l+index_p)/2;
 
     if(index_l > index_p){
-        cout<<"element nieodnaleziony"<<endl;
+        cout<<"element "<<szukany<<" nieodnaleziony"<<endl;
         return;
     }
 
@@ -17,7 +17,7 @@ void wyszukiwanie_binarne(int* tab, int index_l, int index_p, int szukany){
         cout<<"element "<<szukany<<" znajduje sie na miejscu o indexie: "<<srodek<<endl;
         return;
     }else if(szukany < tab[srodek]){
-        wyszukiwanie_binarne(tab, 0, srodek, szukany);
+        wyszukiwanie_binarne(tab, 0, srodek-1, szukany);
     }
     else if(szukany > tab[srodek]){
         wyszukiwanie_binarne(tab, srodek+1, index_p, szukany);
@@ -26,7 +26,7 @@ void wyszukiwanie_binarne(int* tab, int index_l, int index_p, int szukany){
 
 int main()
 {
-    for(int i = 2; i <=20; i+=2){
+    for(int i = -1; i <=20; i++){
         wyszukiwanie_binarne(tab, 0, 9, i);
     }
     return 0;
