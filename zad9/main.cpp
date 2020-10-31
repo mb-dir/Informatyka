a5f0c0319b5a72cb3d370e0a36b75d502fb61e4c
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-10-31 22:24:58 +0100

    Zadanie nr 9

---
+++ zad9/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "**zad9**" << endl;
+
+
+    for(int i = 3; i <=22; i++){
+        cout<<"5*"<<i<<" = "<<5*i<<endl;
+    }
+    return 0;
+}
