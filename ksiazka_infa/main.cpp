a5062bf2950e1a674c72e5cb070dd902ded45546
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-12-03 22:26:15 +0100

    Usunięcie zbędnego kodu

--- ksiazka_infa/main.cpp
+++ ksiazka_infa/main.cpp
@@ -23,45 +23,7 @@ int main()
     Hobbit.cena = 39,99;
 
     cout << "Cena ksiazki to: "<< Hobbit.cena<< endl;
-==>>ksiazka Hobbit = {"Hobbit","Rolingston",1967,39.99};
-    return 0;
-}
-****************************************************************
-
-
-#include <iostream>
-#include <string.h>
-#include <cstdlib>
-
-using namespace std;
-
-// deklaracja struktury
-struct osoba {
-    string imie;
-    string nazwisko;
-    int wiek;
-};
-
-int main()
-{
-    osoba uczniowie[5]; // tablica ze struktury
-
-    for (int i = 0; i<5; i++)
-    {
-        // pobieramy dane
-        cout << "Podaj imie ucznia nr : " << i+1 << endl;
-        cin >> uczniowie[i].imie;
-    }
-
-    for (int i = 0; i<5; i++)
-    {
-        // wyœwietlamy tablicê
-        cout << uczniowie[i].imie << endl;
-    }
-
-    system("pause >nul");
-
     return 0;
 }
 
-ksiazka Hobbit = {"Hobbit","Rolingston",1967,39.99};
+
