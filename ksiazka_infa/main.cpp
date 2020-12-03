aa404e6df89c2b717349ec4ce4a862b36e3e4076
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-12-03 22:36:44 +0100

    Zadanie - wersja podstawowa

--- ksiazka_infa/main.cpp
+++ ksiazka_infa/main.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+string autor, tytul, wydawnictwo;
+int rok_wydania;
+float cena;
+
 // deklaracja struktury
 struct ksiazka {
     string autor;
@@ -14,13 +18,29 @@ struct ksiazka {
 
 int main()
 {
+    cout<<"Podaj autora: ";
+    cin>>autor;
+
+    cout<<"Podaj tytul: ";
+    cin>>tytul;
+
+    cout<<"Podaj rok wydania: ";
+    cin>>rok_wydania;
+
+    cout<<"Podaj wydawnictwo: ";
+    cin>>wydawnictwo;
+
+    cout<<"Podaj cene: ";
+    cin>>cena;
+
+
     ksiazka Hobbit; //tworzenie obiektu struktury o nazwie Hobbit;
 
-    Hobbit.tytul = "Hobbit";
-    Hobbit.autor = "Rolingston";
-    Hobbit.rokwydania = 1967;
-    Hobbit.wydawnictwo = "Amber";
-    Hobbit.cena = 39,99;
+    Hobbit.tytul = tytul;
+    Hobbit.autor = autor;
+    Hobbit.rokwydania = rok_wydania;
+    Hobbit.wydawnictwo = wydawnictwo;
+    Hobbit.cena = cena;
 
     cout << "Cena ksiazki to: "<< Hobbit.cena<< endl;
     return 0;
