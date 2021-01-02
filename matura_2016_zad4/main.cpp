bd235586eba1fb541e97834ed293de4047beb607
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-01-02 20:41:22 +0100

    4.1

--- matura_2016_zad4/main.cpp
+++ matura_2016_zad4/main.cpp
@@ -4,13 +4,24 @@
 
 using namespace std;
 
-int tab[20000], i=0;
+int tabX[10000], tabY[10000];
 
 bool czy_punkt_nalezy_do_kola(int x, int y){
     int r = 200, sx=200, sy=200;//to co wiemy z tresci zadania
 
     int warunek = ((x-sx)*(x-sx)+(y-sy)*(y-sy));
-    if(warunek <= r*r){
+    if(warunek < 40000){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool czy_punkt_nalezy_do_brzegu_kola(int x, int y){
+    int r = 200, sx=200, sy=200;//to co wiemy z tresci zadania
+
+    int warunek = ((x-sx)*(x-sx)+(y-sy)*(y-sy));
+    if(warunek == 40000){
         return true;
     }else{
         return false;
@@ -25,20 +36,30 @@ int main()
 
     punkty.open("./punkty.txt", ios::in);
 
+    int i = 1;
     while(!punkty.eof()){
         getline(punkty,wspolrzedna);
         stringstream sString(wspolrzedna);
         sString >> x;
         sString >> y;
 
-        tab[i]=std::stoi(x);
-        i++;
-        tab[i]=std::stoi(y);
+        tabX[i]=std::stoi(x);
+        tabY[i]=std::stoi(y);
         i++;
     }
 
     punkty.close();
 
     //tab[19999]; - to ostatnia wspó³rzedna z pliku
+
+    int ilosc = 0;
+    for(int i = 0; i<10000; i++){
+        if(czy_punkt_nalezy_do_brzegu_kola(tabX[i], tabY[i])){
+            cout<<"Punkt: ("<<tabX[i]<<","<<tabY[i]<<") nalezy do brzegu okregu"<<endl;
+        }else if(czy_punkt_nalezy_do_kola(tabX[i], tabY[i])){
+            ilosc+=1;
+        }
+    }
+    cout<<ilosc;
     return 0;
 }
