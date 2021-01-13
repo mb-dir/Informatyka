691e7420e0757ef3e45a00ebf22fc47a0ba7a19d
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-01-13 21:13:11 +0100

    6.1

--- pixele_2017/main.cpp
+++ pixele_2017/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int tab[200][320];
 
+int maximum = 0, minimum = 250;
+
 int main()
 {
     fstream pixele;
@@ -14,8 +16,19 @@ int main()
     for(int i = 0; i < 200; i++){
         for(int j = 0; j < 320; j++){
             pixele>>tab[i][j];
+
+            if(tab[i][j] > maximum){
+                maximum = tab[i][j];
+            }
+
+            if(tab[i][j] < minimum){
+                minimum = tab[i][j];
+            }
         }
     }
     pixele.close();
+
+    cout<<maximum<<endl;
+    cout<<minimum<<endl;
     return 0;
 }
