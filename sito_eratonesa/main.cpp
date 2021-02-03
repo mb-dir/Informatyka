3b1b29246fac80363f41ef95a7f37e4a10daf3f5
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-02-03 20:52:32 +0100

    Wybór przedziału

--- sito_eratonesa/main.cpp
+++ sito_eratonesa/main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
-int t[99];
+int n;
 int main()
 {
-    for(int i = 2; i <100; i++){
+    cout<<"Do ilu liczb:";
+    cin>>n;
+
+    int t[n+1];
+    for(int i = 2; i <=n; i++){
         t[i]=i+1;
     }
 
 
-    for(int i=2; i <=10; i++){
+    for(int i=2; i <=sqrt(n); i++){
         if(t[i]!=0){
-            for(int j=i*i; j <=100; j+=i){
+            for(int j=i*i; j <=n; j+=i){
                 t[j]=0;
             }
         }
     }
 
-    for(int i = 2; i <=100; i++){
+    for(int i = 2; i <=n; i++){
         if(t[i]!=0){
             cout<<i<<endl;
         }
