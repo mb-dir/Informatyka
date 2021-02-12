90de965f2fc736ea33fbb8e3a7500a988249878a
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-02-12 22:56:37 +0100

    Algorytm sprawdzający czy liczba jest pierwsza

---
+++ spoj_liczby_pierwsze/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+bool czy_pierwisza(int a){
+    if(a<=2){
+        return true;
+    }
+    for(int i = 2; i <=sqrt(a); i++){
+        if(a%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    cout << czy_pierwisza(7) << endl;
+    return 0;
+}
