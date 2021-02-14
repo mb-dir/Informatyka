aeba83e244e81475b506f96f3ab241080f317c93
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-02-14 22:08:40 +0100

    Inna wersja

--- spoj_liczby_pierwsze/main.cpp
+++ spoj_liczby_pierwsze/main.cpp
@@ -2,23 +2,20 @@
 #include <math.h>
 
 using namespace std;
-bool czy_pierwisza(int a){
-    if(a==2){
-        return true;
-    }else if(a==1){
-        return false;
-    }
-    for(int i = 2; i <=sqrt(a); i++){
-        if(a%i==0){
-            return false;
-        }
-    }
-    return true;
+bool czy_pierwsza(int n)
+{
+	if(n<2)
+		return false; //gdy liczba jest mniejsza ni¿ 2 to nie jest pierwsz¹
+
+	for(int i=2;i*i<=n;i++)
+		if(n%i==0)
+			return false; //gdy znajdziemy dzielnik, to dana liczba nie jest pierwsza
+	return true;
 }
 int main()
 {
     for(int n = 1; n <10000; n++){
-        if(czy_pierwisza(n)){
+        if(czy_pierwsza(n)){
             cout<<"TAK"<<endl;
         }else{
             cout<<"NIE"<<endl;
