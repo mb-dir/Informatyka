0fc7204be4fd09abb76e3135e5ed08e7bd829911
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-02-16 17:27:52 +0100

    euklides na podstawie schematu z internetu

---
+++ euklides_wiki/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int a,b;
+
+int main()
+{
+    cin>>a;
+    cin>>b;
+
+    while(a!=b){
+        if(a>b){
+            a=a-b;
+        }else{
+            b=b-a;
+        }
+    }
+    cout<<a;
+    return 0;
+}
