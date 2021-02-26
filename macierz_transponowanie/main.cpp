cb72a9e013a9750c9404106647cabb8c80293043
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-02-26 22:17:58 +0100

    Tworzenie macierzy

---
+++ macierz_transponowanie/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int kolumny, wiersze, x;
+
+int main()
+{
+    srand( time( NULL ) );
+    kolumny = ( std::rand() % 5 ) + 1;
+    wiersze = ( std::rand() % 5 ) + 1;
+
+
+    int tab[kolumny][wiersze];
+
+    for(int i = 0; i < wiersze; i++){
+        for(int j = 0; j < kolumny; j++){
+            tab[j][i] =( std::rand() % 50 ) + 1;
+        }
+    }
+    cout<<endl;
+    cout<<"Macierz przed"<<endl;
+    for(int i = 0; i < wiersze; i++){
+        for(int j = 0; j < kolumny; j++){
+            if(j == kolumny - 1){
+                cout<<tab[j][i]<<endl;
+            }else{
+                cout<<tab[j][i]<<" ";
+            }
+        }
+    }
+    return 0;
+}
