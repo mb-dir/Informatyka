7067f808ae1bbd14c35150e90582d18cd30cb445
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-03-08 22:27:28 +0100

    Zabawa z odczytem

--- przygotowania_do_matury/main.cpp
+++ przygotowania_do_matury/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
-
+int tab[2];
+string s;
 int main()
 {
+    ifstream dane;
+
+    dane.open("./dane.txt", ios::in);
+
+    while(!dane.eof()){
+        getline(dane,s);
+        stringstream sString(s);
+        sString >> tab[0];
+        sString >> tab[1];
+        cout<<tab[0]<<", "<<tab[1]<<endl;
+    }
+
     cout << "Hello world!" << endl;
+
+    dane.close();
     return 0;
 }
