e128c5304aadc07c53779dba24274023ec334cec
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-03-09 11:29:32 +0100

    Bardziej jasna nazwa zmiennej

--- dziwna_podzielnosc_przez_7/main.cpp
+++ dziwna_podzielnosc_przez_7/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int n, dwie_ostatnie, dwie_ostatnie_mod=0;
+int n, dwie_ostatnie, dwie_ostatnie_mod_suma=0;
 
 int main()
 {
@@ -10,11 +10,11 @@ int main()
     cin>>n;
     while(n){
         dwie_ostatnie = n%100;
-        dwie_ostatnie_mod += dwie_ostatnie%7;
+        dwie_ostatnie_mod_suma += dwie_ostatnie%7;
         n=(n/100)*2;
     }
 
-    if(dwie_ostatnie_mod%7==0){
+    if(dwie_ostatnie_mod_suma%7==0){
         cout<<"PRAWDA"<<endl;
     }else{
         cout<<"FALSZ"<<endl;
