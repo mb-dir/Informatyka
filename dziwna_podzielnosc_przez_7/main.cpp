ecc6438c6c11affd69041dfaea5fb3854b1f1912
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-03-09 11:28:12 +0100

    Działający skrypt

---
+++ dziwna_podzielnosc_przez_7/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int n, dwie_ostatnie, dwie_ostatnie_mod=0;
+
+int main()
+{
+    cout << "Podaj liczbe" << endl;
+    cin>>n;
+    while(n){
+        dwie_ostatnie = n%100;
+        dwie_ostatnie_mod += dwie_ostatnie%7;
+        n=(n/100)*2;
+    }
+
+    if(dwie_ostatnie_mod%7==0){
+        cout<<"PRAWDA"<<endl;
+    }else{
+        cout<<"FALSZ"<<endl;
+    }
+
+    return 0;
+}
