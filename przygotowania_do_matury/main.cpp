640027788df7eaa954dda81551435f2e3c7c4b94
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-03-10 19:40:36 +0100

    Zapis do pliku

--- przygotowania_do_matury/main.cpp
+++ przygotowania_do_matury/main.cpp
@@ -8,27 +8,34 @@ int tab[2];
 string s;
 
 //przyk³ad pierwszy - sprawdziæ, czy druga liczba jest kwadretem pierwszej
-void przyklad_1(int a, int b){
+bool przyklad_1(int a, int b){
     if(a*a == b){
-        cout<<"Tak "<<a<<", "<<b<<endl;
+        return true;
+    }else{
+        return false;
     }
 }
 
 int main()
 {
-    ifstream dane;
+    fstream dane;
+    fstream dane_zapis;
 
     dane.open("./dane.txt", ios::in);
+    dane_zapis.open("./dane_zapis.txt", ios::out);
 
     while(!dane.eof()){
         getline(dane,s);
         stringstream sString(s);
         sString >> tab[0];
         sString >> tab[1];
-        przyklad_1(tab[0], tab[1]);
+        if(przyklad_1(tab[0], tab[1])){
+            dane_zapis<<tab[0]<<" "<<tab[1]<<endl;
+        }
     }
 
 
     dane.close();
+    dane_zapis.close();
     return 0;
 }
