1c69b4a5912152f86089c01ae9cf3cf33d8a1e6f
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-03-15 21:51:45 +0100

    Zapisywanie danych do osobnych tablic

--- pary_matura/main.cpp
+++ pary_matura/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
+string s;
+int liczby[100];
+int i = 0;
+string wyrazy[100];
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    fstream pary;
+
+    pary.open("./pary.txt", ios::in);
+
+    while(!pary.eof()){
+        getline(pary, s);
+        stringstream sString(s);
+        sString >> liczby[i];
+        sString >> wyrazy[i];
+
+        i++;
+    }
+
+    pary.close();
+
+    for(int i = 0; i < 100; i++){
+        cout<<liczby[i]<<endl;
+    }
     return 0;
 }
