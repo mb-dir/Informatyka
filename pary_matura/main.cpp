60e2b0b5a636951b997336ae35924869911cf1bc
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-03-15 22:17:19 +0100

    Funkcje do podpunktu nr 1

--- pary_matura/main.cpp
+++ pary_matura/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <math.h>
 
 using namespace std;
 
@@ -9,8 +10,42 @@ int liczby[100];
 int i = 0;
 string wyrazy[100];
 
+bool czy_pierwsza(int a){
+    if(a == 1){
+        return false;
+    }
+    for(int i = 2; i <= sqrt(a); i++){
+        if(a%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+void hipotezaG(int a){
+    int maxr = 0;
+    int r = 0;
+    int para_liczb[2];
+
+    if((a > 4) && (a %2 == 0)){
+        for(int i = 3; i <= a/2; i+=2){
+            if(czy_pierwsza(i) && czy_pierwsza(a-i)){
+                r=abs(i- (a-i));
+                if(r >= maxr){
+                    maxr = r;
+                    para_liczb[0] = i;
+                    para_liczb[1] = a-i;
+                }
+            }
+        }
+    }
+    cout<<para_liczb[0]<<" "<<para_liczb[1]<<endl;
+}
+
+
 int main()
 {
+    hipotezaG(24);
     fstream pary;
 
     pary.open("./pary.txt", ios::in);
