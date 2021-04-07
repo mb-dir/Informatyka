486e00e4bd71a0e3ed29e44864badde5105df699
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-04-07 22:51:51 +0200

    Funkcja sprawdzająca warunek zad 1

--- matura2019/main.cpp
+++ matura2019/main.cpp
@@ -8,6 +8,17 @@ string tab_liczby_string[500];
 int tab[500];
 
 int jd = 0;
+
+bool czy_pot_3(int a){
+    int b = 1;
+    for(int i = 1; i < 12; i++){
+        if(a == b){
+            return true;
+        }
+        b*=3;
+    }
+    return false;
+}
 int main()
 {
     fstream liczby;
@@ -24,5 +35,9 @@ int main()
         tab[i] = std::stoi(tab_liczby_string[i]);
     }
 
+    cout<<czy_pot_3(1);
+    cout<<czy_pot_3(2);
+    cout<<czy_pot_3(9);
+    cout<<czy_pot_3(2187);
     return 0;
 }
