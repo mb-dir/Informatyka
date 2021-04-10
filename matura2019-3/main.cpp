c1995d5436433e133978a7690a4f8ca7e2c1f57a
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-04-10 22:38:20 +0200

    NWD

--- matura2019-3/main.cpp
+++ matura2019-3/main.cpp
@@ -3,14 +3,28 @@
 
 using namespace std;
 string linia;
+
+int nwd(int a, int b){
+    while(a!=b){
+        if(a>b){
+            a=a-b;
+        }else{
+            b=b-a;
+        }
+    }
+    return a;
+}
+
 int main()
 {
     fstream liczby;
     liczby.open("./liczby.txt", ios::in);
     while(!liczby.eof()){
         getline(liczby, linia);
-        cout<<linia<<endl;
+        //cout<<linia<<endl;
     }
     liczby.close();
+
+    cout<<nwd(16,24);
     return 0;
 }
