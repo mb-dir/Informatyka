3641e152ab818298eea20fb892a629b840dc72bb
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-04-10 22:35:36 +0200

    Odczyt danych z pliku

---
+++ matura2019-3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+string linia;
+int main()
+{
+    fstream liczby;
+    liczby.open("./liczby.txt", ios::in);
+    while(!liczby.eof()){
+        getline(liczby, linia);
+        cout<<linia<<endl;
+    }
+    liczby.close();
+    return 0;
+}
