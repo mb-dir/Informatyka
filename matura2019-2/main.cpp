0637815efad63f26a0ac76f3ecb520f9e4773c5e
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-04-10 22:16:08 +0200

    Funkcja dla silni

--- matura2019-2/main.cpp
+++ matura2019-2/main.cpp
@@ -3,14 +3,26 @@
 
 using namespace std;
 string linia;
+int silnia(int a){
+    if(a == 0){
+        return 1;
+    }else{
+        return silnia(a-1)*a;
+    }
+}
+void zadanie(int a){
+
+}
+
 int main()
 {
+
+    cout<<silnia(10);
     fstream liczby;
     liczby.open("./liczby.txt", ios::in);
 
     while(!liczby.eof()){
         getline(liczby, linia);
-        cout<<linia<<endl;
     }
 
     liczby.close();
