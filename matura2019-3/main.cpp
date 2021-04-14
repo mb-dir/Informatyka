f3a169e6c6205d68bde85518f367df57d88cc68c
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-04-14 21:56:46 +0200

    3/3

--- matura2019-3/main.cpp
+++ matura2019-3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 string linia;
@@ -16,6 +17,16 @@ int nwd(int a, int b){
         }
     }
     return a;
+}
+
+int max_dzielnik(int a){
+    int maxD;
+    for(int i = 2; i <=a; i++){
+        if(a%i == 0){
+            maxD = i;
+        }
+    }
+    return maxD;
 }
 
 int main()
@@ -51,15 +62,37 @@ int main()
             dl=0;
         }
     }
-    cout<<liczby_tab[koniec-150];
+    //cout<<liczby_tab[koniec-150];
 
     int tab_max[150];
     for(int i = koniec-150, j = 0; i < koniec; i++, j++){
         tab_max[j] = liczby_tab[i];
     }
 
+    int maxD = 0;
     for(int i = 0; i < 150; i++){
-        cout<<tab_max[i]<<endl;
+        if(max_dzielnik(tab_max[i])> maxD){
+            maxD = max_dzielnik(i);
+        }
+    }
+    //cout<<max_dzielnik(tab_max[0])<<endl;
+
+    //wszystkie dzielniki tej liczby i potem sprawdzanie na pale
+    //cout<<tab_max[0];
+    int odp2 = tab_max[0];
+
+    sort(tab_max, tab_max+150);
+
+    //dzielniki 74(najmniejszej liczby z tego przedzialu)
+    int tab_dzielniki[3] = {74,37,2};
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 150; j++){
+            if(tab_max[j] % tab_dzielniki[i] != 0){
+                //nigdy sie niespelnia - wszystkie te liczby sa dzielnikami kazdej z liczb, 74 jest max
+                cout<<tab_dzielniki[i]<<" jest zle dla "<<tab_max[j]<<endl;
+            }
+        }
     }
     return 0;
 }
