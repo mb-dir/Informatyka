7c9c10f0ae37f3cc802cb655ce8a8dcc57a5ee03
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-04-14 20:02:32 +0200

    Znajdowanie lidera

--- lider/main.cpp
+++ lider/main.cpp
@@ -1,20 +1,54 @@
 #include <iostream>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-int tab_test[12] = {3,4,4,4,3,3,3,12,3,3,3,3};
+
+int n = 20;
+int index_srodek_tab = n/2;
+int tab[20];
+int j = 0;
 
 int main()
 {
-    sort(tab_test, tab_test+12);
-    int j = 0;
-    int s = tab_test[12/2];
-    for(int i = 0; i < 12; i++){
-        if(tab_test[i] == s){
+    //Wype³nienie tablicy
+    srand(time(NULL));
+    for(int i = 0; i < n; i++){
+        if(i%2 == 0){
+            tab[i] = 7;
+        }else{
+            tab[i] = rand()%19+1;
+        }
+    }
+
+    //Posortowanie tablicy
+    sort(tab, tab+n);
+    for(int i = 0; i < n; i++){
+        if(tab[i] == tab[index_srodek_tab]){
             j++;
         }
     }
-    cout<<j;
+
+    cout<<"TABLICA PO POSORTOWANIU"<<endl;
+    for(int i = 0; i < n; i++){
+        if(i == 0){
+            cout<<"["<<tab[i]<<", ";
+        }else if (i != n-1){
+            cout<<tab[i]<<", ";
+        }else{
+            cout<<tab[i]<<"]"<<endl;
+            cout<<endl;
+        }
+    }
+
+
+    if(j > index_srodek_tab){
+        cout<<tab[index_srodek_tab]<<" jest liderem, a ciag ma dlugosc: "<<j<<endl;
+    }else{
+        cout<<tab[index_srodek_tab]<<" jest kandydatem na lidera, ale ciag jest zbyt krotki("<<j<<") - musi byc wiekszy od: "<<index_srodek_tab<<endl;
+    }
+
     return 0;
 }
