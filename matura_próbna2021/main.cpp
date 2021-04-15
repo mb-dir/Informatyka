f6d2b7c1bd157fa43c971be1f75a67cc61467ea7
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-04-15 22:23:52 +0200

    Właściwy zapis danych

--- matura_próbna2021/main.cpp
+++ matura_próbna2021/main.cpp
@@ -14,34 +14,39 @@ string linia, skrot, miasto;
 
 int tab_wymiary[140];
 
+galeria galerie[50];
+
 int main()
 {
     fstream dane;
 
     dane.open("./galerie.txt", ios::in);
 
-    do{
+
+    int j = 0;
+    while(!dane.eof()){
         getline(dane, linia);
 
         stringstream sString(linia);
 
         sString>>skrot;
         sString>>miasto;
+
+        galeria galeria_zapis;
+
+        galeria_zapis.skrot = skrot;
+        galeria_zapis.miasto = miasto;
+
         for(int i = 0; i < 140; i++){
-            sString>>tab_wymiary[i];
+            sString>>galeria_zapis.wymiary[i];
         }
-        cout<<linia<<endl;
-    }while(0);
-
-    cout<<endl;
-    cout<<endl;
-    cout<<endl;
-    cout<<skrot<<" "<<miasto;
-
-    for(int i = 0; i < 140; i++){
-        cout<<tab_wymiary[i]<<endl;
+        galerie[j] = galeria_zapis;
+        j++;
     }
-
     dane.close();
+
+    for(int i = 0; i < 50; i++){
+        cout<<galerie[i].wymiary[1]<<endl;
+    }
     return 0;
 }
