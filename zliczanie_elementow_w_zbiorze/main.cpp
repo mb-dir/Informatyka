293726d1a84560cd4904c58ea5c6e9e8e9d3d1c7
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-04-16 21:51:16 +0200

    Gotowy algorytm

---
+++ zliczanie_elementow_w_zbiorze/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string tab[10] = {"C","A","C","C","A","A","G","J","G","C"};
+
+int pierwsze_wystapienie(string a, string* tab, int dl){
+    for(int i = 0; i < dl; i++){
+        if(a == tab[i]){
+            return i;
+        }
+    }
+}
+
+int ostatnie_wystapienie(string a, string* tab, int dl){
+    for(int i = dl-1; i >= 0; i--){
+        if(a == tab[i]){
+            return i;
+        }
+    }
+}
+
+int main()
+{
+    cout << "Przed sortowaniem" << endl;
+    for(int i = 0; i < 10; i ++){
+        cout<<tab[i]<<" ";
+    }
+    sort(tab, tab+10);
+    cout<<endl;
+    cout<<"Po sortowaniu"<<endl;
+    for(int i = 0; i < 10; i ++){
+        cout<<tab[i]<<" ";
+    }
+    cout<<endl;
+
+    cout<<pierwsze_wystapienie("J", tab, 10)<<endl;
+    cout<<ostatnie_wystapienie("J", tab, 10)<<endl;
+
+    cout<<"Ilosc wystapien to: "<<ostatnie_wystapienie("J", tab, 10)+1 - pierwsze_wystapienie("J", tab, 10);
+    return 0;
+}
