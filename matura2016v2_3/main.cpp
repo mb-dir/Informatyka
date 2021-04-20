6c90e877300da053e36b65c0a52815b26626bcc2
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-04-20 22:32:04 +0200

    Próba rozwiązania

--- matura2016v2_3/main.cpp
+++ matura2016v2_3/main.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <math.h>
 
 using namespace std;
 
 string linia;
 
 string w1,w2;
+
+char jeden_cezar(char w, int k){
+    if(int(w + k) > 90){
+        return int(w + k + 26);
+    }else{
+        return int(w + k);
+    }
+}
+char jeden_cezar_opcja_2(char w, int k){
+    if(int(w - k) <60){
+        return int(w - k + 26);
+    }else{
+        return int(w - k);
+    }
+}
+
+bool czy_cezar_zly(string w1, string w2){
+    int k = abs(w1[0]-w2[0]);
+
+    for(int i = 0; i < w1.length(); i++){
+        if(jeden_cezar(w1[i], k) != w2[i] && jeden_cezar_opcja_2(w1[i], k)!= w2[i]){
+            return true;
+        }
+    }
+
+}
 
 int main()
 {
@@ -19,9 +46,12 @@ int main()
         sString>>w1;
         sString>>w2;
 
-        cout<<w1<<" "<<w2<<endl;
-    }
+        //if(czy_cezar_zly(w1,w2)){
+            //cout<<w1<<" "<<w2<<" zly"<<endl;
+        //}
 
+    }
+    cout<<czy_cezar_zly("ZAWISLAK", "EFBNXQFP");
     zad.close();
     return 0;
 }
