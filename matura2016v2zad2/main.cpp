e55a3060e6987937bdaa4ab1174d79f735e7ec71
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-04-20 21:57:37 +0200

    Zapis danych w odpowiedni sposób

--- matura2016v2zad2/main.cpp
+++ matura2016v2zad2/main.cpp
@@ -6,16 +6,27 @@ using namespace std;
 
 string linia;
 
+string slowa[3000];
+int klucz[3000];
+
 int main()
 {
     fstream zad;
 
     zad.open("dane_6_2.txt", ios::in);
+    int i = 0;
     while(!zad.eof()){
         getline(zad, linia);
-        cout<<linia<<endl;
+        stringstream sString(linia);
+        sString>>slowa[i];
+        sString>>klucz[i];
+        i++;
+    }
+    zad.close();
+
+    for(int i = 0; i < 3000; i++){
+        cout<<slowa[i]<<" "<<klucz[i]<<endl;
     }
 
-    zad.close();
     return 0;
 }
