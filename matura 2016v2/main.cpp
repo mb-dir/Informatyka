c95a44d0b7aecf5b6887457d844caec4cca6e732
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-04-20 21:46:20 +0200

    Funkcja cezar

--- matura 2016v2/main.cpp
+++ matura 2016v2/main.cpp
@@ -5,6 +5,18 @@ using namespace std;
 
 string linia_zad1;
 
+void cezar(string a){
+    int k = 107%26;
+    for(int i = 0; i < a.length(); i++){
+        if(int(a[i]) > 87){
+            a[i] = a[i]+k-26;
+        }else{
+            a[i] = a[i]+3;
+        }
+    }
+    cout<<a;
+}
+
 int main()
 {
     fstream zad;
@@ -12,9 +24,11 @@ int main()
 
     while(!zad.eof()){
         getline(zad, linia_zad1);
-        cout<<linia_zad1<<endl;
+        //cout<<linia_zad1<<endl;
     }
 
     zad.close();
+
+    cezar("INTERPRETOWANIEXYZ");
     return 0;
 }
