a62de7e35b1d5bd00ef1e02f854eedb5d4c752c3
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-05-14 11:30:58 +0200

    Test odczyt

--- luzne_do_matury/main.cpp
+++ luzne_do_matury/main.cpp
@@ -1,42 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
+string linia;
 
-int silnia(int a){
-    if(a < 2){
-        return 1;
-    }else{
-        return silnia(a-1)*a;
-    }
+string x,y;
+
+int suma(int a, int b){
+    return a+b;
 }
-
-void suma_cyfr(int a){
-    int s = 0;
-    while(a){
-        s+=a%10;
-        a/=10;
-    }
-
-    cout<<s;
-}
-int tab[5] = {5,2,1,17,17};
 
 int main()
 {
-    for(int i = 0; i < 5; i ++){
-        for(int j = 0; j < 4; j++){
-            if(tab[j] < tab[j+1]){
-                int x = tab[j];
-                tab[j] = tab[j+1];
-                tab[j+1] = x;
-            }
-        }
+    fstream liczby;
+
+    liczby.open("./test.txt", ios::in);
+
+    while(!liczby.eof()){
+        getline(liczby, linia);
+        stringstream sString(linia);
+        sString>>x;
+        sString>>y;
+
+        cout<<suma(std::stoi(x), std::stoi(y))<<endl;
     }
 
-    for(int i = 0; i < 5; i++){
-        cout<<tab[i]<<endl;
-    }
-    suma_cyfr(2137100);
+    liczby.close();
 
     return 0;
 }
