4703b3ebd7900a5de6cf30591f0a45c26797a6ee
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-05-14 12:21:01 +0200

    Praca nad zamianą liczby z dowolnego systemu na 10

--- luzne_do_matury/main.cpp
+++ luzne_do_matury/main.cpp
@@ -15,9 +15,48 @@ int dl_liczby(int x){
     return s;
 }
 
+
+int potega(int podstawa, int wykladnik){
+    if(wykladnik == 0){
+        return 1;
+    }else{
+        return potega(podstawa, wykladnik-1)*podstawa;
+    }
+}
+
+void to_dec(int a, int system){
+    int dl = dl_liczby(a);
+    int dl_temp = dl;
+    int tab_potegi[dl];
+    for(int i = 0; i < dl; i++){
+        tab_potegi[i] = potega(system, dl_temp-1);
+        dl_temp--;
+    }
+    for(int i = 0; i < dl; i++){
+        cout<<tab_potegi[i]<<endl;
+    }
+
+
+
+
+    //rozbijane liczby na cyfry
+
+    int tab_cyfry[dl];
+    int a_temp = a;
+    int dl_temp2 = dl-1;
+
+    while(a_temp){
+        tab_cyfry[dl_temp2] = a_temp%10;
+        dl_temp2--;
+        a_temp/=10;
+    }
+    for(int i = 0; i < dl; i++){
+        cout<<tab_cyfry[i]<<endl;
+    }
+}
+
 int main()
 {
-    cout<<dl_liczby(111121);
-
+    to_dec(2021, 3);
     return 0;
 }
