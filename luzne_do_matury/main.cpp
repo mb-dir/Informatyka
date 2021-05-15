cfef1fdb1cd98d364a3bf516356c3a5e05de90bc
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-05-15 13:37:11 +0200

    Zamiana z lekcji

--- luzne_do_matury/main.cpp
+++ luzne_do_matury/main.cpp
@@ -4,91 +4,21 @@
 
 using namespace std;
 
-int dl_liczby(int x){
-    int s = 0;
-
-    while(x){
-        x/=10;
-        s++;
-    }
-    return s;
-}
-
-
-int potega(int podstawa, int wykladnik){
-    if(wykladnik == 0){
-        return 1;
-    }else{
-        return potega(podstawa, wykladnik-1)*podstawa;
-    }
-}
-
-void to_dec(int a, int system){
-    int dl = dl_liczby(a);
-    int dl_temp = dl;
-    int tab_potegi[dl];
-    for(int i = 0; i < dl; i++){
-        tab_potegi[i] = potega(system, dl_temp-1);
-        dl_temp--;
-    }
-
-    //rozbijane liczby na cyfry
-
-    int tab_cyfry[dl];
-    int a_temp = a;
-    int dl_temp2 = dl-1;
-
-    while(a_temp){
-        tab_cyfry[dl_temp2] = a_temp%10;
-        dl_temp2--;
-        a_temp/=10;
-    }
-
-
-    int liczba_dec = 0;
-    for(int i = 0; i < dl; i++){
-        int x = tab_cyfry[i]* tab_potegi[i];
-
-        liczba_dec+=x;
-    }
-    cout<<liczba_dec;
-}
-
-
 void from_dec(int liczba, int system){
-
-    int dl;
-
-    for(int i = 0; i <100; i++){
-        if(liczba < potega(system, i)){
-            dl=i;
-            break;
+    string s;
+    while(liczba){
+        int x = liczba%system;
+        if(x < 10){
+            s = char('0' + x)+s;
+        }else{
+            s = char('A'-10 + x)+s;
         }
+        liczba/=system;
     }
-
-    int potegi[dl];
-    int nowa_liczba[dl];
-
-    for(int i = 0; i < dl; i++){
-        potegi[i] = potega(system, i);
-    }
-
-    for(int i = dl-1; i >-1; i--){
-        int x;
-        x = liczba/potegi[i];
-        nowa_liczba[i] = x;
-        liczba-=potegi[i]*x;
-    }
-
-
-    for(int i = dl-1; i >-1; i--){
-        cout<<nowa_liczba[i];
-    }
-
+    cout<<s;
 }
-
 int main()
 {
-    from_dec(137, 4);
+    from_dec(8, 2);
     return 0;
 }
