90ee0db5be9e28004183b4ca096183ab1a78f719
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-05-15 13:49:32 +0200

    Z lekcji

--- luzne_do_matury/main.cpp
+++ luzne_do_matury/main.cpp
@@ -17,8 +17,18 @@ void from_dec(int liczba, int system){
     }
     cout<<s;
 }
+
+
+void to_dec(int liczba, int system){
+    string s;
+    while(liczba){
+        s=char(48+liczba%system)+s;
+        liczba/=system;
+    }
+    cout<<s;
+}
 int main()
 {
-    from_dec(8, 2);
+    to_dec(10, 2);
     return 0;
 }
