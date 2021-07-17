02235545bed105e2b294cfb9bc833dfe4dd90d86
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-07-17 23:10:35 +0200

    Rozwiązanie

--- spoj_pesel/main.cpp
+++ spoj_pesel/main.cpp
@@ -2,31 +2,34 @@
 
 using namespace std;
 
-int t, suma = 0;
+int t;
 
 string zadanie(string pesel){
+    int liczba = 0;
+    int suma = 0;
     for(int j = 0; j < 11; j++){
-            int liczba;
-            if( (j == 1) || (j == 5) || (j == 9)){
-                //Zamiana ¿eby ta pojedyñcza cyfra faktycznie by³a intem - https://stackoverflow.com/questions/5029840/convert-char-to-int-in-c-and-c
-                liczba = (char(pesel[j]) - '0')*3;
-            }else if(j == 2 || j == 6){
-                liczba = (char(pesel[j]) - '0')*7;
-            }else if(j == 3 || j == 7){
-                liczba = (char(pesel[j]) - '0')*9;
-            }else{
-                liczba = (char(pesel[j]) - '0')*1;
-            }
-            suma+=liczba;
+        if( (j == 1) || (j == 5) || (j == 9)){
+            //Zamiana ¿eby ta pojedyñcza cyfra faktycznie by³a intem - https://stackoverflow.com/questions/5029840/convert-char-to-int-in-c-and-c
+            liczba = (char(pesel[j]) - '0')*3;
+        }else if(j == 2 || j == 6){
+            liczba = (char(pesel[j]) - '0')*7;
+        }else if(j == 3 || j == 7){
+            liczba = (char(pesel[j]) - '0')*9;
+        }else{
+            liczba = (char(pesel[j]) - '0')*1;
         }
-        if(suma <= 0){
-            return "";
-        }
-        if(suma%2 == 0 && suma % 5 == 0){
+        suma+=liczba;
+    }
+
+    if(suma > 0){
+        if(suma % 10 == 0){
             return "D";
         }else{
             return "N";
         }
+    }else{
+        return "N";
+    }
 }
 
 int main()
