51a24bbaa5d8c4c85ffb7ec7424bc757ebdff41f
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-07-26 22:10:02 +0200

    Zmiana wyśwetlania strumienia wyjściowego
    Pdejrzewam, że problem jest wciąż przy dużych liczbch

--- spoj_potęgowanie/main.cpp
+++ spoj_potęgowanie/main.cpp
@@ -16,19 +16,26 @@ int main()
 {
     cin>>d;
 
-    for(int i = 0; i < d; i++){
-        cin>>a>>b;
+    int tab[2*d];
 
-        if(b >= 10){
-            if(a > 10){
-                cout<<a%10<<endl;
-            }else if(a < 10){
-                cout<<a<<endl;
+    for(int i = 0; i < 2*d; i+=2){
+        cin>>a>>b;
+        tab[i] = a;
+        tab[i+1] = b;
+    }
+
+
+    for(int i = 0; i < 2*d; i+=2){
+        if(tab[i+1] >= 10){
+            if(tab[i] > 10){
+                cout<<tab[i]%10<<endl;
+            }else if(tab[i] < 10){
+                cout<<tab[i]<<endl;
             }else{
                 cout<<0;
             }
         }else{
-            int x = potegowanie(a,b);
+            int x = potegowanie(tab[i],tab[i+1]);
             if(x > 10){
                 cout<<x%10<<endl;
             }else if (x<10){
