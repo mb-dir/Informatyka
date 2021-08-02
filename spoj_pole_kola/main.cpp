a46f4a492ecc19bf86c007f11f56d9b145423fa6
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-02 22:36:32 +0200

    Dobre rozwiązanie

--- spoj_pole_kola/main.cpp
+++ spoj_pole_kola/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
-long double r,d;
-float pi = 3.141592654;
+float r,d;
+long double pi = 3.141592654;
 int main()
 {
     cin>>r>>d;
 
+    int y = (r*r-((d*d)/4))*pi;
+
     long double x = (r*r-((d*d)/4))*pi;
-    cout<<x<<endl;
+    cout << fixed << setprecision(2) << x;
 
     return 0;
 }
