c6be1068123ee57438a3f9bff1ff05ded45384fa
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-06 15:27:36 +0200

    Rozwiązanie

--- spoj_podzielnosc/main.cpp
+++ spoj_podzielnosc/main.cpp
@@ -4,20 +4,28 @@ using namespace std;
 
 int t;
 
+void solve(int n, int x, int y){
+    for(int i = 2; i < n; i++){
+        if(i%x==0 && i%y!=0){
+            cout<<i<<" ";
+        }
+    }
+}
+
 int main()
 {
     cin>>t;
 
+    int tab[t][3];
     for(int i = 0; i < t; i++){
-        int n,x,y;
-
-        cin>>n>>x>>y;
-
-        for(int j = 0; j < n; j++){
-            if(j%x ==  0 && j%y != 0){
-                cout<<j<<endl;
-            }
+        for(int j = 0; j < 3; j++){
+            cin>>tab[i][j];
         }
+    }
+
+    for(int i = 0; i < t; i++){
+        solve(tab[i][0], tab[i][1], tab[i][2]);
+        cout<<endl;
     }
     return 0;
 }
