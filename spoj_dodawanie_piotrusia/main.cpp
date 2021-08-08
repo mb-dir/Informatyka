2b5ad5550a0651288b3b6e77dcaff2754c67d3d2
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-08 17:03:45 +0200

    v1

--- spoj_dodawanie_piotrusia/main.cpp
+++ spoj_dodawanie_piotrusia/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+
 
 using namespace std;
 
@@ -12,9 +14,39 @@ bool czy_palindrom(string s){
     return true;
 }
 
+int dodaj_palindrom(int a){
+    std::string s = std::to_string(a);
+    int len = s.length();
+    for(int i = 0; i < len/2; i++){
+        int x = s[i];
+        s[i] = s[len-1-i];
+        s[len-1-i] = x;
+    }
+
+    return a + std::stoi(s);
+}
 
 int main()
 {
-    cout<<czy_palindrom("AGGGA");
+
+    int a;
+    cin>>a;
+
+    if(a < 10){
+    cout<<a<<" "<<0;
+        return 0;
+    }
+    int c = 0;
+    while(1){
+        std::string s = std::to_string(a);
+
+        if(czy_palindrom(s)){
+            cout<<a<<" "<<c;
+            return 0;
+        }else{
+            a = dodaj_palindrom(a);
+        }
+        c++;
+    }
     return 0;
 }
