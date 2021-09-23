1aba25d906a7117093a99c73cff4258132439c15
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-09-23 16:33:50 +0200

    Wersja z pokazaniem przebiegu sortowania

--- qs_try/main.cpp
+++ qs_try/main.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 int arr[6] = {10,1,14,7,-1,0};
 
+void drawArray(int*arr, int l,int r){
+    for(int i = l; i < r; i++){
+        if(i == l){
+            cout<<"["<<arr[i]<<", ";
+        }else if(i == r-1){
+            cout<<arr[i]<<"]"<<endl;
+        }else{
+            cout<<arr[i]<<", ";
+        }
+    }
+}
 
 int partial(int*arr, int l, int r){
     //r(last element) as a pivot
@@ -19,12 +30,15 @@ int partial(int*arr, int l, int r){
     int y = arr[i+1];
     arr[i+1] = arr[r];
     arr[r] = y;
+    cout<<"l: "<<l<<" r: "<<r<<"  ";
+    drawArray(arr, 0, 6);
 
     return i+1;
 }
 
 void qs(int*arr, int l, int r){
     if(l>=r){
+        cout<<"odrzucone l: "<<l<<" odrzucone r: "<<r<<endl;
         return;
     }else{
         int p = partial(arr, l, r);
@@ -35,13 +49,9 @@ void qs(int*arr, int l, int r){
 
 int main()
 {
-    for(int i = 0; i < 6; i++){
-        cout<<arr[i]<<endl;
-    }
-    cout<<endl;
+
+    drawArray(arr, 0, 6);
     qs(arr, 0, 5);
-    for(int i = 0; i < 6; i++){
-        cout<<arr[i]<<endl;
-    }
+    //drawArray(arr, 0, 6);
     return 0;
 }
