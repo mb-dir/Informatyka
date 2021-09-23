49e2fc66b47d50438bff63088c15478348a03e27
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-09-23 14:49:07 +0200

    Wersja poprawiona - działa

--- qs_try/main.cpp
+++ qs_try/main.cpp
@@ -7,7 +7,8 @@ int arr[6] = {10,1,14,7,-1,0};
 int partial(int*arr, int l, int r){
     //r(last element) as a pivot
     int i = l-1;
-    for(int j = l; j < r-1; j++){
+    //<= than r-1 cuz the last element before the pivot must be taken into account
+    for(int j = l; j <= r-1; j++){
         if(arr[j] < arr[r]){
             i+=1;
             int x = arr[i];
@@ -34,6 +35,10 @@ void qs(int*arr, int l, int r){
 
 int main()
 {
+    for(int i = 0; i < 6; i++){
+        cout<<arr[i]<<endl;
+    }
+    cout<<endl;
     qs(arr, 0, 5);
     for(int i = 0; i < 6; i++){
         cout<<arr[i]<<endl;
