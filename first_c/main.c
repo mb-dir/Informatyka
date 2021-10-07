166daba95123a6fd5cc8634c3c152e9c9006b9ec
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-07 20:39:05 +0200

    Other way of showing the table

--- first_c/main.c
+++ first_c/main.c
@@ -7,11 +7,7 @@ int main()
     for(int i = 1; i < 11; i++){
         for(int j = 1; j < 11; j++){
             int liczba = i*j;
-            if(liczba < 10){
-              printf("%d  ",i*j);
-            }else{
-                printf("%d ",i*j);
-            }
+            printf("%d\t", liczba);
         }
         printf("\n");
     }
