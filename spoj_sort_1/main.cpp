5e0549d55218cba9930522ec87b88a69f2f11404
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-10-08 12:56:51 +0200

    Try to sort the points

--- spoj_sort_1/main.cpp
+++ spoj_sort_1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string.h>
+#include <math.h>
 
 using namespace std;
 
@@ -8,8 +9,10 @@ int t;
 struct point {   // Declare PERSON struct type
     int X;
     long Y;
+    int R;
     string ID;
 };
+
 
 int main()
 {
@@ -32,13 +35,25 @@ int main()
 
             newPoint.X = x;
             newPoint.Y = y;
+            newPoint.R = abs(x*y);
             newPoint.ID = id;
 
             tabPkt[j] = newPoint;
         }
+        for(int p = 0; p < n; p++){
+            for(int c = 0; c < n-1; c++){
+                cout<<"R dla "<<tabPkt[c].ID<<": "<<tabPkt[c].R<<", R dla "<<tabPkt[c+1].ID<<": "<<tabPkt[c+1].R<<endl;
+                if(tabPkt[c].R > tabPkt[c+i].R){
+                    cout<<"Zamiana"<<endl;
+                    point z = tabPkt[c];
+                    tabPkt[c] = tabPkt[c+1];
+                    tabPkt[c+1] = z;
+                }
+            }
+        }
 
         for(int j = 0; j < n; j++){
-            cout<<tabPkt[j].ID<<" ("<<tabPkt[j].X<<", "<<tabPkt[j].Y<<")"<<endl;
+            cout<<tabPkt[j].ID<<" ("<<tabPkt[j].X<<", "<<tabPkt[j].Y<<"), r: "<<tabPkt[j].R<<endl;
         }
     }
     return 0;
