c3813e6984cd5c3553729c7e741e4a410d0b4793
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-11-26 22:28:26 +0100

    Dynamically alocate 2D array

---
+++ spoj_transponowanie_macierzy/spoj_transponowanie_macierzy/spoj_transponowanie_macierzy.c
@@ -0,0 +1,24 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	int a=0, b=0;
+	scanf_s("%d %d", &a,&b);
+	int* A = (int*)malloc(a * b * sizeof(int));
+
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++) {
+			int c;
+			scanf_s("%d", &c);
+			*(A + i * b + j) = c;
+		}
+	}
+
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++) {
+			printf("%d ", *(A + i * b + j));
+		}printf("\n");
+	}
+
+	return 0;
+}
