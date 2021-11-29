e9be480b14659a8a7a4097e846e8a70f2e1a3340
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-11-29 22:25:29 +0100

    cezar in C

---
+++ spoj_cezarC/cezar/cezar.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char* w="dupa";
+	int k;
+
+	printf("Podaj przesuniecie: ");
+	scanf_s("%d", &k);
+
+	int key = k % 26;
+
+	while(*w){
+		int a = *w;
+		if (a + key > 122) {
+			a = a + key - 26;
+		}
+		else {
+			a += key;
+		}
+		printf("%c", a);
+		*w++;
+	}
+	return 0;
+}
